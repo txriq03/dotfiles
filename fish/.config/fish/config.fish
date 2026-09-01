1d9107009efa798178eef44722239e0bd25403f5
Tariq Brown <tariq.brown@triadcreative.co.uk>
2026-09-01 09:54:12 +0100

    add yazi bind + edit_command_buffer bind

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	starship init fish | source
-	zoxide init fish | source
-	abbr -a v 'nvim'
-	abbr -a ai 'opencode'
-	abbr -a lg 'lazygit'
-	abbr -a ip 'ipconfig getifaddr en0'
-	abbr -a ls 'eza'
-	bind \cx\ce edit_command_buffer
+    starship init fish | source
+    zoxide init fish | source
+    abbr -a v nvim
+    abbr -a ai opencode
+    abbr -a lg lazygit
+    abbr -a ip 'ipconfig getifaddr en0'
+    abbr -a y yazi
+    abbr -a ls eza
+    bind \ee edit_command_buffer
 end
 
 # opencode
